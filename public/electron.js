a9b89ac2057b42dc644bb2805f7a124758d1a37f
brennop <brennopcordeiro@gmail.com>
2020-01-07 16:28:23 -0300

    :tada: Initial commit

---
+++ public/electron.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    frame: false,
+  });
+
+  // and load the index.html of the app.
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
